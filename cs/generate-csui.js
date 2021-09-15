697ce7d41943fd808dfe8cf4f38f248a48d0652a
xushunfa459 <xushunfa459@hellobike.com>
2021-09-15 22:45:56 +0800

    feat: update

--- cs/generate-csui.js
+++ cs/generate-csui.js
@@ -3,19 +3,16 @@ const config = require('../src/config.json');
 const path = require('path');
 const fs = require('fs-extra');
 let importStr = '';
-const packages = [];
 
 config.nav.map((item) => {
   item.packages.forEach((element) => {
     let { name, show, type, exportEmpty } = element;
     if (show || exportEmpty) {
       importStr += `import './__VUE/${name.toLowerCase()}/cs-${name.toLocaleLowerCase()}.mjs';\n`;
-      packages.push(name);
     }
   });
 });
 
-console.log(importStr, packages, 89); // [Button,Card]
 
 let fileStr = `${importStr}`;
 
--- src/sites/mobile/main.ts
+++ src/sites/mobile/main.ts
@@ -1,11 +1,10 @@
+// mobile ui 在 web 端可视化展示
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import Csui from '@/packages/csui.vue';
 import '@/sites/assets/styles/reset.scss';
 import '@/packages/utils/touchEmulator';
 
 createApp(App)
   .use(router)
-  // .use(Csui)
   .mount('#app');
