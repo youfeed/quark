364e2865aee4a91bba2c819353e6d71b356757d0
xushunfa459 <xushunfa459@hellobike.com>
2021-09-18 16:59:20 +0800

    feat: update

---
+++ 0.0.17/intro.0cb89a26.js
@@ -0,0 +1,1 @@
+import{e,o,g as s}from"./vendor.61e79af0.js";const t={class:"markdown-body"},a=[s("h1",null,"Cubeshop",-1),s("h4",null,"Cubeshop 是一个基于 Web Components 的跨框架 UI 组件库，输出标准的 Custom Element，组件可以同时在 React、Vue、Preact、Angular 或 原生 JS 等任意框架或无框架中使用。",-1)],n={setup:(s,{expose:n})=>(n({frontmatter:{}}),(s,n)=>(o(),e("div",t,a)))};export{n as default};
